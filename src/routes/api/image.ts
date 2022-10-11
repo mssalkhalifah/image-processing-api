ab1047ebe4e67d21a420b2ce27b6cfecfbf6f722
mssalkhalifah <mssalkhalifah@gmail.com>
2022-10-11 21:39:03 +0300

    Added image resize functionality

--- src/routes/api/image.ts
+++ src/routes/api/image.ts
@@ -1,11 +1,12 @@
 import express from 'express';
 import path from 'path';
 import fs from 'fs';
+import sharp from 'sharp';
 
 type QueryParams = {
   filename: string;
-  width: number;
-  height: number;
+  width: string;
+  height: string;
 };
 
 const router = express.Router();
@@ -13,20 +14,53 @@ const router = express.Router();
 router.get(
   '/image',
   async (req: express.Request<{}, {}, {}, QueryParams>, res): Promise<void> => {
-    const url = path.join(
-      global.publicRoot,
-      'images',
-      req.query.filename.concat('.jpg'),
-    );
+    if (!req.query.filename) {
+      res.sendStatus(400);
+    } else {
+      const url = path.join(
+        global.publicRoot,
+        'images',
+        req.query.filename.concat('.jpg'),
+      );
 
-    try {
-      if (fs.existsSync(url)) {
-        res.status(200).sendFile(url);
+      if (req.query.width || req.query.height) {
+        const imageBuffer = new Uint8Array(
+          (await sharp(url).toBuffer()).buffer,
+        );
+        const imageMetadata = sharp(imageBuffer).metadata();
+
+        const imageWidth: number = req.query.width
+          ? parseInt(req.query.width, 10)
+          : (await imageMetadata).width!;
+        const imageHeight: number = req.query.height
+          ? parseInt(req.query.height, 10)
+          : (await imageMetadata).height!;
+
+        const newFilename = req.query.filename.concat(
+          imageWidth.toString(),
+          imageHeight.toString(),
+          '.jpg',
+        );
+
+        const filepath = path.resolve(global.publicRoot, 'thumb', newFilename);
+
+        sharp(imageBuffer)
+          .resize(imageWidth, imageHeight)
+          .toFile(filepath)
+          .then((): void => {
+            res.status(200).sendFile(filepath);
+          });
       } else {
-        res.sendStatus(404);
+        try {
+          if (fs.existsSync(url)) {
+            res.status(200).sendFile(url);
+          } else {
+            res.sendStatus(404);
+          }
+        } catch (err) {
+          res.sendStatus(500);
+        }
       }
-    } catch (err) {
-      res.sendStatus(500);
     }
   },
 );
--- src/tests/routes/api/imageSpec.ts
+++ src/tests/routes/api/imageSpec.ts
@@ -1,27 +1,86 @@
 import request from 'supertest';
-// import sh from 'sharp';
+import sharp from 'sharp';
+import path from 'path';
+import fs from 'fs';
 import app from '../../../index';
 
 describe('GET /api/image', (): void => {
-  const filename = 'mountain';
+  const filename = 'milkyway';
 
-  it('should get status 200 if request succeeded', async (): Promise<void> => {
+  it('should respone with status 200 if request succeeded', async (): Promise<void> => {
     const response = await request(app).get(`/api/image?filename=${filename}`);
     expect<number>(response.statusCode).toEqual(200);
   });
 
-  it('should get status 404 if file not found', async (): Promise<void> => {
+  it('should respone with status 404 if file not found', async (): Promise<void> => {
     const response = await request(app).get(
       '/api/image?filename=impossibleImage',
     );
     expect<number>(response.statusCode).toEqual(404);
   });
-  // it('should respone with the same file as the filename quryed', async (): Promise<void> => {
-  //   const respone = await request(app).get(`/api/image?filename=${filename}`);
-  //   const responseImage = new Uint32Array(
-  //     (await sh(respone.body).raw().toBuffer()).buffer,
-  //   );
-  //   // for (var i = 0; i < responseImage.length; i++) {}
-  //   expect<number>(responseImage.length).toEqual(10);
-  // });
+
+  it('should respone with status 400 if no parameter was provided', async (): Promise<void> => {
+    const response = await request(app).get('/api/image');
+    expect<number>(response.statusCode).toEqual(400);
+  });
+
+  it('should respone with the same image requested', async (): Promise<void> => {
+    const response = await request(app).get(`/api/image?filename=${filename}`);
+    const responeImage = new Uint8Array(
+      (await sharp(response.body).toBuffer()).buffer,
+    );
+    const expectedImage = new Uint8Array(
+      (
+        await sharp(
+          path.resolve(global.publicRoot, 'images', filename.concat('.jpg')),
+        ).toBuffer()
+      ).buffer,
+    );
+
+    let isSame = responeImage.length === expectedImage.length;
+    if (isSame) {
+      for (let i = 0; i < responeImage.length; i += 1) {
+        if (responeImage[i] !== expectedImage[i]) {
+          isSame = false;
+          break;
+        }
+      }
+    }
+
+    expect<boolean>(isSame).toBeTrue();
+  });
+
+  describe('sharp generated image', (): void => {
+    const width: number = 200;
+    const height: number = 200;
+    let response: request.Response;
+    let responseImageMetadata: sharp.Metadata;
+
+    beforeAll(async (): Promise<void> => {
+      response = await request(app).get(
+        `/api/image?filename=${filename}&width=${width}&height=${height}`,
+      );
+      responseImageMetadata = await sharp(response.body).metadata();
+    });
+
+    it('should respone with an image of the same height queried', (): void => {
+      expect<number | undefined>(responseImageMetadata.height).toEqual(height);
+    });
+
+    it('should respone with an image of the same width queried', (): void => {
+      expect<number | undefined>(responseImageMetadata.width).toEqual(width);
+    });
+
+    it(`should generate a file with the same name as ${filename}${width}${height}.jpg in thumb`, (): void => {
+      expect<boolean>(
+        fs.existsSync(
+          path.resolve(
+            global.publicRoot,
+            'thumb',
+            `${filename}${width}${height}.jpg`,
+          ),
+        ),
+      ).toBeTrue();
+    });
+  });
 });
